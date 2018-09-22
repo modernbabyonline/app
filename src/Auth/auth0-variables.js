c2a37be8e0d8bac471864a5497c83b918217531d
Bevan Hunt <bevan@bevanhunt.com>
2018-09-21 23:08:06 -0700

    add callback for prod

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'modernbaby.auth0.com',
   clientId: 'C2e7X2CGEdYrRn3W4gyJmypUprdnJGK1',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://app.modernbaby.online/callback'
 }
