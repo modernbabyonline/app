36f3b2bb7d7f586c473062f5360c1a18e5f0ccfb
Bevan Hunt <bevan@bevanhunt.com>
2018-10-02 01:26:13 -0700

    try setting audience

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -8,6 +8,7 @@ export default class Auth {
     clientID: AUTH_CONFIG.clientId,
     redirectUri: AUTH_CONFIG.callbackUrl,
     responseType: 'token id_token',
+    audience: 'https://api.modernbaby.online/',
     scope: 'openid email profile'
   });
 
