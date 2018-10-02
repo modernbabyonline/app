272d3479e8743fa2cabafe729d1511348950519b
Ladoli <villadolidangeloryan@gmail.com>
2018-10-01 23:16:50 -0700

    Update callback link

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,7 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'modernbaby.auth0.com',
   clientId: 'C2e7X2CGEdYrRn3W4gyJmypUprdnJGK1',
-  // callbackUrl: 'https://app.modernbaby.online/callback'
-  callbackUrl: 'http://localhost:3000/callback'
-
+  callbackUrl: 'https://app.modernbaby.online/callback'
+  // callbackUrl: 'http://localhost:3000/callback'
 }
